fb87cc166321a7f76c83fb614d8f8b2a1d09354e
Paulo Sergio <pauloserg861@gmail.com>
2025-02-22 14:22:06 -0300

    test: ensure a Product is created with all data

--- src/domain/entities/product.spec.ts
+++ src/domain/entities/product.spec.ts
@@ -32,4 +32,24 @@ describe('Product Entity', () => {
     const sut = makeSut();
     expect(sut.created_at).toBeInstanceOf(Date);
   });
+
+  it('should be able to create a product with all data', () => {
+    const sut = makeSut();
+    sut['imagePath'] = 'imagePath.jpg';
+    sut['description'] = 'Any Product Description';
+    sut['ingredients'] = ['ingredient1', 'ingredient2'];
+    sut['categories'] = ['category1', 'category2'];
+    sut['created_at'] = new Date('2025-01-01');
+
+    const props = {
+      name: 'Product Name',
+      price: 25,
+      imagePath: 'imagePath.jpg',
+      description: 'Any Product Description',
+      ingredients: ['ingredient1', 'ingredient2'],
+      categories: ['category1', 'category2'],
+      created_at: new Date('2025-01-01'),
+    };
+    expect(sut).toStrictEqual(expect.objectContaining(props));
+  });
 });
