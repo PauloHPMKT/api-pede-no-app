3c9bc215281b7adc278c49cdfc038af5d6930ddd
Paulo Sergio <pauloserg861@gmail.com>
2025-02-22 14:12:49 -0300

    feat: ensure a Product is created with name and price

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -1,1 +1,17 @@
-export class Product {}
+export class Product {
+  public name: string;
+  public price: number;
+  public imagePath?: string;
+  public description?: string;
+  public ingredients?: string[];
+  public categories?: string[];
+
+  constructor(props: Partial<Product>) {
+    Object.assign(this, props);
+
+    this.imagePath = props.imagePath ?? null;
+    this.description = props.description ?? null;
+    this.ingredients = props.ingredients ?? [];
+    this.categories = props.categories ?? [];
+  }
+}
