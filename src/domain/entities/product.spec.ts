d661b2c7fb92185476cca38edd6fe68a1683b978
Paulo Sergio <pauloserg861@gmail.com>
2025-02-22 14:13:04 -0300

    test: ensure a Product is created with name and price

--- src/domain/entities/product.spec.ts
+++ src/domain/entities/product.spec.ts
@@ -1,8 +1,30 @@
 import { Product } from './Product';
 
+const makeSut = (): Product => {
+  const props = {
+    name: 'Product Name',
+    price: 25,
+  };
+  return new Product(props);
+};
+
 describe('Product Entity', () => {
   it('should be defined', () => {
-    const sut = new Product();
+    const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should be able to creat a product with a name and price', () => {
+    const sut = makeSut();
+    expect(sut).toStrictEqual(
+      expect.objectContaining({
+        name: 'Product Name',
+        price: 25,
+        imagePath: null,
+        description: null,
+        ingredients: [],
+        categories: [],
+      }),
+    );
   });
 });
