bf826aaf7bde420012dcf2605c19aabc95ff4803
Paulo Sergio <pauloserg861@gmail.com>
2025-02-22 14:35:53 -0300

    feat: ensure CreateProductController return 400 if no name is provided

--- src/presentation/controllers/create-product.ts
+++ src/presentation/controllers/create-product.ts
@@ -1,1 +1,22 @@
-export class CreateProductController {}
+interface HttpRequest {
+  body?: any;
+  headers?: any;
+  params?: any;
+  file?: any;
+}
+
+interface HttpResponse {
+  statusCode: number;
+  body: any;
+}
+
+export class CreateProductController {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+  }
+}
